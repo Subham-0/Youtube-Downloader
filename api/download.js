d25beccfefc96744c746c44344610558472d2d82
Subham-0 <dashsubham2002@gmail.com>
2024-10-22 11:45:14 -0700

    check download.js

--- api/download.js
+++ api/download.js
@@ -1,12 +1,15 @@
 const ytdl = require('ytdl-core');
 
-export default async function handler(req, res) {
-    const videoUrl = req.query.url;
+export default function handler(req, res) {
+    res.status(200).json({ message: "API is working!" });
+  }
+// export default async function handler(req, res) {
+//     const videoUrl = req.query.url;
 
-    if (!videoUrl || !ytdl.validateURL(videoUrl)) {
-        return res.status(400).json({ success: false, message: 'Invalid URL' });
-    }
+//     if (!videoUrl || !ytdl.validateURL(videoUrl)) {
+//         return res.status(400).json({ success: false, message: 'Invalid URL' });
+//     }
 
-    res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
-    ytdl(videoUrl, { format: 'mp4' }).pipe(res);
-}
+//     res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
+//     ytdl(videoUrl, { format: 'mp4' }).pipe(res);
+// }
